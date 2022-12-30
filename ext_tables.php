151e94039e826591d34914418fa1597aa55af512
maulik <maulik.nitsan@gmail.com>
2022-12-30 12:51:52 +0530

    [TODO]Implement Basic Module

--- ext_tables.php
+++ ext_tables.php
@@ -5,3 +5,28 @@ defined('TYPO3') || die();
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_nst3dev_domain_model_productarea', 'EXT:ns_t3dev/Resources/Private/Language/locallang_csh_tx_nst3dev_domain_model_productarea.xlf');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_nst3dev_domain_model_productarea');
 })();
+
+if (TYPO3_MODE === 'BE') {
+    $reportController = 'Report';
+    if (version_compare(TYPO3_branch, '10.0', '>=')) {
+        $reportController = \NITSAN\NsT3dev\Controller\ProductAreaController::class;
+    }
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'NITSAN.NsT3dev',
+        'nitsan',
+        't3dev',
+        '',
+        [
+            $reportController => 'list, show, new, edit, update',
+        ],
+        [
+            'access' => 'user,group',
+            'icon'   => 'EXT:ns_t3dev/Resources/Public/Icons/ext_domain product.png',
+            'labels' => 'LLL:EXT:ns_t3dev/Resources/Private/Language/locallang.xlf:module.name',
+            'inheritNavigationComponentFromMainModule' => false
+        ]
+    );
+
+    $GLOBALS['TCA']['tx_nsfeedback_domain_model_feedbacks']['ctrl']['hideTable'] = 1;
+    $GLOBALS['TCA']['tx_nsfeedback_domain_model_report']['ctrl']['hideTable'] = 1;
+}
